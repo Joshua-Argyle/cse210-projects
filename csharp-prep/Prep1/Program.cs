b1dc36ddbf62a02dbedcdbdc9928ca7bf415aa6f
Joshua Argyle <joshbudd33@gmail.com>
2025-09-20 12:04:23 -0600

    Completed Prep 1 Assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+        // Console.WriteLine("Hello Prep1 World!");
+
+        // string firstName = "Bob";
+        // Console.WriteLine($"Hello {firstName}");
+
+        // Console.Write("What is your favorite color? ");
+        // string favoriteColor = Console.ReadLine();
+
+        // Console.WriteLine($"{firstName}, your favorite color is {favoriteColor}. ");
+
+        // Console.Write($"How old are you? ");
+        // int age = int.Parse(Console.ReadLine());
+        // Console.WriteLine($"{firstName}, you are {age} years old.");
+
+        // int a = 13;
+        // int b = 7;
+
+        // int c = a + b;
+
+        // Console.WriteLine($"c is {c}.");
+        // Console.WriteLine($"a/b is (a / b)");
+        // Console.WriteLine($"{(double)a / b}");
+
+
+
+
     }
 }
