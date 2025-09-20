ded428e9846fdf154b13b056af75b640cc6378b9
Joshua Argyle <joshbudd33@gmail.com>
2025-09-20 13:08:26 -0600

    Completed prep 2 assignment.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,60 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string sign = "";
+        string letter = "grade";
+        Console.Write("What is your grade? ");
+        int grade = int.Parse(Console.ReadLine());
+        if (grade >= 94)
+        {
+            sign = "";
+            letter = "A";
+        }
+        else if (grade <= 59)
+        {
+            sign = "";
+            letter = "F";
+        }
+        else
+        {
+            if (grade >= 90)
+            {
+                letter = "A";
+            }
+            else if (grade >= 80 && grade < 90)
+            {
+                letter = "B";
+            }
+            else if (grade >= 70 && grade < 80)
+            {
+                letter = "C";
+            }
+            else if (grade >= 60 && grade < 70)
+            {
+                letter = "D";
+            }
+
+            if ((grade % 10) >= 7 && (grade % 10) <= 9)
+            {
+                sign = "+";
+            }
+            else if ((grade % 10) >= 0 && (grade % 10) <= 3)
+            {
+                sign = "-";
+            }
+        }
+        Console.WriteLine($"{letter}{sign}");
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congrats, you passed the class. ");
+        }
+        else
+        {
+            Console.WriteLine("I'm sorry, try again next time! ");
+        }
     }
 }
