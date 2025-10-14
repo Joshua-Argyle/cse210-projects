8d908c88694eddf855f22db5954fd088397023a4
Joshua Argyle <joshbudd33@gmail.com>
2025-10-14 16:49:45 -0600

    Finished Learning 3 Assignment

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,46 @@
+using System;
+class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int numerator)
+    {
+        _top = numerator;
+        _bottom = 1;
+    }
+    public Fraction(int numerator, int denominator)
+    {
+        _top = numerator;
+        _bottom = denominator;
+    }
+    public int GetTop()
+    {
+        return _top;
+    }
+    public void SetTop(int numerator)
+    {
+        _top =  numerator;
+    }
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+    public void SetBottom(int denominator)
+    {
+        _bottom = denominator;
+    }
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction test1 = new Fraction();
+        Fraction test2 = new Fraction(5);
+        Fraction test3 = new Fraction(2, 3);
+        // test1.SetBottom(5);
+        // Console.WriteLine(test1.GetBottom());
+        // Console.WriteLine(test3.GetDecimalValue());
+        // Console.WriteLine(test3.GetFractionString());
+        // test3.SetBottom(5);
+        // Console.WriteLine(test3.GetDecimalValue());
+        // Console.WriteLine(test3.GetFractionString());
+        Console.WriteLine(test1.GetFractionString());
+        Console.WriteLine(test1.GetDecimalValue());
+        Console.WriteLine(test2.GetFractionString());
+        Console.WriteLine(test2.GetDecimalValue());
+        Console.WriteLine(test3.GetFractionString());
+        Console.WriteLine(test3.GetDecimalValue());
+
     }
 }
